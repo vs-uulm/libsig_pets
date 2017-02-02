c3a87f26ea04b025c15065ac43770bbdf5407be7
Linus Kramer <linus.kramer@uni-ulm.de>
2017-02-02 23:36:57 +0100

    changed interface according to review

--- test/test_RenHarn.py
+++ test/test_RenHarn.py
@@ -13,15 +13,15 @@ class TestElGamal(unittest.TestCase):
         """This is a special function which is called before each of
         our tests."""
         # We set up the keys and params
-        (self.pubkey, self.privkey, self.generator, self.prime) = ElGamal.keygen(keysize)
+        (self.pubkey, self.privkey) = ElGamal.keygen(keysize)
 
     def test_sign_and_verify(self):
         """
         Test if we can verify a signed message.
         """
         message = str.encode("Star wars is awesome")
-        signature = ElGamal.sign(self.privkey, message, self.generator, self.prime)
-        self.assertTrue(ElGamal.verify(self.pubkey, message, signature, self.generator, self.prime))
+        signature = ElGamal.sign(self.privkey, message)
+        self.assertTrue(ElGamal.verify(self.pubkey, message, signature))
 
 
 class TestRenHarn(unittest.TestCase):
@@ -33,11 +33,11 @@ class TestRenHarn(unittest.TestCase):
         """This is a special function which is called before each of
         our tests."""
         # We set up the keys and params
-        (e, d, self.generator, self.prime) = RenHarn.keygen(keysize)
+        (e, d) = RenHarn.keygen(keysize)
         self.privkeys = [d]
         self.pubkeys = [e]
         for _ in range(15):
-            (e, d, _, _) = RenHarn.keygen(keysize, self.generator, self.prime)
+            (e, d) = RenHarn.keygen(keysize, self.pubkeys[0][1], self.pubkeys[0][2])
             self.privkeys.append(d)
             self.pubkeys.append(e)
 
@@ -48,47 +48,46 @@ class TestRenHarn(unittest.TestCase):
         n = len(self.pubkeys)
 
         message = str.encode("Star wars is awesome")
-        (_, i_0, v_i_0, sig) = RenHarn.ringsign(self.privkeys[0], self.pubkeys, message, self.generator, self.prime)
-        self.assertTrue(RenHarn.verify(self.pubkeys, i_0, v_i_0, message, sig, self.generator, self.prime))
+        sig = RenHarn.ringsign(self.privkeys[0], self.pubkeys, message)
+        self.assertTrue(RenHarn.verify(self.pubkeys, message, sig))
 
     def test_wrong_idx(self):
         n = len(self.pubkeys)
         message = str.encode("Star wars is awesome")
-        (_, i_0, v_i_0, sig) = RenHarn.ringsign(self.privkeys[0], self.pubkeys, message, self.generator, self.prime)
+        (i_0, v_i_0, ms) = RenHarn.ringsign(self.privkeys[0], self.pubkeys, message)
         for i in range(n-1):
-            self.assertFalse(RenHarn.verify(self.pubkeys, (i_0+i+1) % n, v_i_0, message, sig, self.generator, self.prime))
+            sig = ((i_0+i+1) % n, v_i_0, ms)
+            self.assertFalse(RenHarn.verify(self.pubkeys, message, sig))
 
     def test_wrong_pubkey(self):
         n = len(self.pubkeys)
         message = str.encode("Star wars is awesome")
-        (_, i_0, v_i_0, sig) = RenHarn.ringsign(self.privkeys[0], self.pubkeys, message, self.generator, self.prime)
+        sig = RenHarn.ringsign(self.privkeys[0], self.pubkeys, message)
         pk = self.pubkeys
-        pk[n-1] = 12345678765432943458754347854345689843456723974982891365294562364 % self.prime
-        self.assertFalse(RenHarn.verify(pk, i_0, v_i_0, message, sig, self.generator, self.prime))
+        pk[n-1] = (12345678765432943458754347854345689843456723974982891365294562364 % self.pubkeys[0][2], pk[n-1][1], pk[n-1][2])
+        self.assertFalse(RenHarn.verify(pk, message, sig))
 
     def test_wrong_signature(self):
         pubkeys = self.pubkeys
         n = len(pubkeys)
         m = str.encode("Star wars is awesome")
-        g = self.generator
-        p = self.prime
-        (_, i_0, v_i_0, sig) = RenHarn.ringsign(self.privkeys[0], pubkeys, m, g, p)
+        sig = RenHarn.ringsign(self.privkeys[0], pubkeys, m)
         for i in range(n):
             for magic_number in [-1, 0, 1]:
-                (m, alpha, beta) = sig[i]
-                sig[i] = ((m + magic_number) % n, alpha, beta)
-                self.assertFalse(RenHarn.verify(pubkeys, i_0, v_i_0, m, sig, g, p))
-                sig[i] = (m, (alpha + magic_number) % n, beta)
-                self.assertFalse(RenHarn.verify(pubkeys, i_0, v_i_0, m, sig, g, p))
-                sig[i] = (m, alpha, (beta + magic_number) % n)
-                self.assertFalse(RenHarn.verify(pubkeys, i_0, v_i_0, m, sig, g, p))
-                sig[i] = (magic_number % n, alpha, beta)
-                self.assertFalse(RenHarn.verify(pubkeys, i_0, v_i_0, m, sig, g, p))
-                sig[i] = (m, magic_number % n, beta)
-                self.assertFalse(RenHarn.verify(pubkeys, i_0, v_i_0, m, sig, g, p))
-                sig[i] = (m, alpha, magic_number % n)
-                self.assertFalse(RenHarn.verify(pubkeys, i_0, v_i_0, m, sig, g, p))
-                sig[i] = (m, alpha, beta)
+                (m, alpha, beta) = sig[2][i]
+                sig[2][i] = ((m + magic_number) % n, alpha, beta)
+                self.assertFalse(RenHarn.verify(pubkeys, m, sig))
+                sig[2][i] = (m, (alpha + magic_number) % n, beta)
+                self.assertFalse(RenHarn.verify(pubkeys, m, sig))
+                sig[2][i] = (m, alpha, (beta + magic_number) % n)
+                self.assertFalse(RenHarn.verify(pubkeys, m, sig))
+                sig[2][i] = (magic_number % n, alpha, beta)
+                self.assertFalse(RenHarn.verify(pubkeys, m, sig))
+                sig[2][i] = (m, magic_number % n, beta)
+                self.assertFalse(RenHarn.verify(pubkeys, m, sig))
+                sig[2][i] = (m, alpha, magic_number % n)
+                self.assertFalse(RenHarn.verify(pubkeys, m, sig))
+                sig[2][i] = (m, alpha, beta)
 
 
 if __name__ == 'main':
